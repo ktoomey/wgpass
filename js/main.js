479b96a4f8ff7e571c01c41f2c319f3e8dc11d74
johnpolacek <johnpolacek@gmail.com>
2013-04-18 14:36:07 -0500

    initial prototype

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function() {
+	var connectors = [' ','-','.','~','+'];
+	function getRandomConnector() {
+		return connectors[Math.floor(Math.random()*connectors.length)];
+	}
+	$.getJSON('data/actions.json',function(data) {
+		var actions = data.actions.sort(function() { return 0.5 - Math.random(); });
+		$('#manly-action').text(actions[0]);
+		$('#manly-action-generate').text(actions[1]);
+	});
+	$.getJSON('data/passwords.json',function(data) {
+		var password =  '' +
+						data.first.sort(function() { return 0.5 - Math.random(); })[0] +
+						getRandomConnector() +
+						data.second.sort(function() { return 0.5 - Math.random(); })[0] +
+						getRandomConnector() +
+						data.third.sort(function() { return 0.5 - Math.random(); })[0] +
+						getRandomConnector() +
+						data.numbers.sort(function() { return 0.5 - Math.random(); })[0];
+		$('.output').val(password);
+		$('#output-contain').removeClass('hidden');
+	});
+});
