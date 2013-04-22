58ce57ed1cbb6ea19c0c6f423a788a4e67e340d5
johnpolacek <johnpolacek@gmail.com>
2013-04-22 10:12:02 -0500

    nno full page refresh on button click

--- js/main.js
+++ js/main.js
@@ -3,22 +3,32 @@ $(function() {
 	function getRandomConnector() {
 		return connectors[Math.floor(Math.random()*connectors.length)];
 	}
-	$.getJSON('data/actions.json',function(data) {
-		var actions = data.actions.sort(function() { return 0.5 - Math.random(); });
-		$('#manly-action').text(actions[0]);
-		$('#manly-action-generate').text(actions[1]);
-	});
-	$.getJSON('data/passwords.json',function(data) {
-		var password =  '' +
-						data.first.sort(function() { return 0.5 - Math.random(); })[0] +
-						getRandomConnector() +
-						data.second.sort(function() { return 0.5 - Math.random(); })[0] +
-						getRandomConnector() +
-						data.third.sort(function() { return 0.5 - Math.random(); })[0] +
-						getRandomConnector() +
-						data.numbers.sort(function() { return 0.5 - Math.random(); })[0];
-		$('.output').val(password);
-		$('#output-contain').removeClass('hidden');
-	});
+	function generatePassword() {
+		$.getJSON('data/actions.json',function(data) {
+			var actions = data.actions.sort(function() { return 0.5 - Math.random(); });
+			$('#manly-action').text(actions[0]);
+			$('#manly-action-generate').text(actions[1]);
+		});
+		$.getJSON('data/passwords.json',function(data) {
+			var password =  '' +
+							data.first.sort(function() { return 0.5 - Math.random(); })[0] +
+							getRandomConnector() +
+							data.second.sort(function() { return 0.5 - Math.random(); })[0] +
+							getRandomConnector() +
+							data.third.sort(function() { return 0.5 - Math.random(); })[0] +
+							getRandomConnector() +
+							data.numbers.sort(function() { return 0.5 - Math.random(); })[0];
+			$('.output').val(password);
+			$('#output-contain').removeClass('hidden');
+		});
+	}
+	
 	$('.output').on('click',function() {$(this).select();});
+	$('.big-button').on('click',function(e) {
+		e.preventDefault();
+		generatePassword();
+	});
+
+	generatePassword();
+
 });
